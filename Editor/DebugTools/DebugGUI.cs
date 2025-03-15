49d20331b009bff914ef832c21dca6354f9229c7
Lev <66064249+Metsker@users.noreply.github.com>
2025-03-15 11:34:30 +0300

    Add files via upload

---
+++ Editor/DebugTools/DebugGUI.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Globalization;
+using ToolbarUtils.UnityToolbarExtender;
+using UnityEditor;
+using UnityEngine;
+
+namespace ToolbarUtils.DebugTools
+{
+    [InitializeOnLoad]
+    public static class DebugGUI
+    {
+        public static bool DebugMode {
+            get => EditorPrefs.GetInt("DebugMode", 0) == 1;
+            private set => EditorPrefs.SetInt("DebugMode", value ? 1 : 0);
+        }
+        
+        static DebugGUI()
+        {
+            ToolbarExtender.RightToolbarGUI.Add(OnToolbarGUI);
+        }
+
+        private static void OnToolbarGUI()
+        {
+            GUILayout.Space(5);
+            
+            DrawDebugToggle();
+            
+            GUILayout.Space(10);
+
+            DrawTimescaleSlider();
+        }
+
+        private static void DrawDebugToggle() =>
+            DebugMode = GUILayout.Toggle(DebugMode, new GUIContent("Debug", "Enables debug mode"), GUILayout.ExpandWidth(false));
+
+        private static void DrawTimescaleSlider()
+        {
+            GUILayout.Label("TimeScale", GUILayout.ExpandWidth(false));
+            Time.timeScale = (float)Math.Round(GUILayout.HorizontalSlider(Time.timeScale, 0, 2, GUILayout.Width(100), GUILayout.ExpandWidth(false)), 1);
+            GUILayout.Label(Time.timeScale.ToString(CultureInfo.InvariantCulture));
+        }
+    }
+}
---
+++ Editor/SceneNavigation/SceneNavGUI.cs
@@ -0,0 +1,55 @@
+﻿using System.IO;
+using JetBrains.Annotations;
+using ToolbarUtils.UnityToolbarExtender;
+using UnityEditor;
+using UnityEditor.SceneManagement;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace ToolbarUtils.SceneNavigation
+{
+    [InitializeOnLoad]
+    public static class SceneNavGUI
+    {
+        private static bool Redirect {
+            get => EditorPrefs.GetBool("RedirectScene");
+            set => EditorPrefs.SetBool("RedirectScene", value);
+        }
+        
+        static SceneNavGUI()
+        {
+            ToolbarExtender.LeftToolbarGUI.Add(OnToolbarGUI);
+        }
+
+        private static void OnToolbarGUI()
+        {
+            GUILayout.FlexibleSpace();
+            GUILayout.BeginHorizontal();
+
+            foreach (EditorBuildSettingsScene scene in EditorBuildSettings.scenes)
+            {
+                string path = scene.path;
+                string name = Path.GetFileNameWithoutExtension(path);
+
+                if (!GUILayout.Button(new GUIContent(name, "Hold Alt to open in additive mode")))
+                    continue;
+                
+                if (!EditorSceneManager.SaveCurrentModifiedScenesIfUserWantsTo())
+                    return;
+                        
+                EditorSceneManager.OpenScene(path, !Keyboard.current.altKey.isPressed ? OpenSceneMode.Single : OpenSceneMode.Additive);
+            }
+            
+            Redirect = GUILayout.Toggle(Redirect, new GUIContent("Redirect", "Redirect to the first scene in build on play"));
+            SetPlayModeStartScene(Redirect ? EditorBuildSettings.scenes[0].path : null);
+            
+            GUILayout.EndHorizontal();
+        }
+
+        private static void SetPlayModeStartScene([CanBeNull]string scenePath)
+        {
+            SceneAsset startScene = string.IsNullOrEmpty(scenePath) ? null : AssetDatabase.LoadAssetAtPath<SceneAsset>(scenePath);
+            EditorSceneManager.playModeStartScene = startScene;
+        }
+    }
+}
